cd7c207143c3bd9dfd6ff60ebf8d27e62a15b8fd
Bruno Gomes <brunogomesdeveloper@gmail.com>
2020-11-25 22:23:32 -0300

    Readme ofuscador

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# ofuscador
+# Ofuscador
+
+ + Renomear  aqruivo .env-exemplo para .env .
+
+ + Alterar no arquivo .env os parâmetros PASTA_ORIGEM e PASTA_DESTINO 
+
+ + Alterar parâmetro OPTIONSPRESET com o nível do ofuscamento
+
+
+Instalar pacotes
+
+``` js
+npm install
+``` 
+
+Executar script ofuscador
+
+``` js
+npm run ofuscar
+``` 
+
--- ofuscar.js
+++ ofuscar.js
@@ -5,13 +5,13 @@ const path = require('path');
 
 var JavaScriptObfuscator = require('javascript-obfuscator');
 
-fs.readdirSync('./' + process.env.PASTA_ORIGEM + '/').forEach(file => {
+fs.readdirSync(  process.env.PASTA_ORIGEM ).forEach(file => {
 
     if (path.extname(file) == ".js") {
 
         console.log("Arquivo encontrado: ", file)
 
-        fs.readFile('./' + process.env.PASTA_ORIGEM + '/' + file, 'utf8', function (err, data) {
+        fs.readFile(process.env.PASTA_ORIGEM + '/'  + file, 'utf8', function (err, data) {
 
             if (err) {
                 console.log("Erro arquivo: ", file, err)
@@ -19,7 +19,7 @@ fs.readdirSync('./' + process.env.PASTA_ORIGEM + '/').forEach(file => {
 
             let obfuscationResult = JavaScriptObfuscator.obfuscate(data, { optionsPreset: process.env.OPTIONSPRESET });
 
-            fs.writeFile('./' + process.env.PASTA_DESTINO + '/' + file, obfuscationResult.getObfuscatedCode(), function (err) {
+            fs.writeFile( process.env.PASTA_DESTINO + '/' + file, obfuscationResult.getObfuscatedCode(), function (err) {
                 if (err) {
                     return console.log("Erro ao ofuscar:", file, err);
                 }
